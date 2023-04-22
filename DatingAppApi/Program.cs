7f9a4e2d4cb505746e576204a4e823abaf9f7e0e
holumyne <olumideakinmoladun@gmail.com>
2023-04-22 18:47:00 +0100

    second commit

--- DatingAppApi/Program.cs
+++ DatingAppApi/Program.cs
@@ -48,6 +48,7 @@ else
 
     // Parse connection URL to connection string for Npgsql
     connUrl = connUrl.Replace("postgres://", string.Empty);
+
     var pgUserPass = connUrl.Split("@")[0];
     var pgHostPortDb = connUrl.Split("@")[1];
     var pgHostPort = pgHostPortDb.Split("/")[0];
@@ -63,6 +64,40 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseNpgsql(connString);
 });
+
+
+//builder.Services.AddDbContext<DataContext>(options =>
+//{
+//    var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+//    string connStr;
+
+//    if (env == "Development")
+//    {
+//        connStr = builder.Configuration.GetConnectionString("DefaultConnection"); // DefaultConnection string must match appsettings.Development.json
+//    }
+//    else
+//    {
+//        var connUrl = Environment.GetEnvironmentVariable("HEROKU_POSTGRESQL_SILVER_URL");
+
+//        connUrl = connUrl.Replace("postgres://", string.Empty);
+
+//        var pgUserPass = connUrl.Split("@")[0];
+//        var pgHostPortDb = connUrl.Split("@")[1];
+//        var pgHostPort = pgHostPortDb.Split("/")[0];
+//        var pgDb = pgHostPortDb.Split("/")[1];
+//        var pgUser = pgUserPass.Split(":")[0];
+//        var pgPass = pgUserPass.Split(":")[1];
+//        var pgHost = pgHostPort.Split(":")[0];
+//        var pgPort = pgHostPort.Split(":")[1];
+
+//        connString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};sslmode=Prefer;Trust Server Certificate=true";
+//        connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+//    }
+
+//    options.UseNpgsql(connStr);
+//});
+
 
 builder.Services.AddHealthChecks();
 var app = builder.Build();
